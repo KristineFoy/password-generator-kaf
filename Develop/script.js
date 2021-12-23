a7cec2b7892ab42da9a97fb2e5f5474ce47eb2ea
KristineFoy <tisstoy@gmail.com>
2021-12-22 23:07:32 -0800

    first upload

---
+++ Develop/script.js
@@ -0,0 +1,48 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+function generatePassword() {
+// 1 password criteria ask
+// 1a length 8-128
+// 1b lower, upper, number, special
+// 2 imput valadtion, a least one 4 loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return"generator password will go here";
+}
+
+
+
+// Write password to the #password input
+// 3 valid input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+document.querySelector('#password').pass;
+console.log ('#password');
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+// 4 display password
+generateBtn.addEventListener("click", writePassword);
