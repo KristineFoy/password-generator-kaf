83f3d857266a1eb6f06eac49a38b24f8da59ae1e
KristineFoy <tisstoy@gmail.com>
2021-12-23 14:29:15 -0800

    working demo

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,57 @@
-// Assignment Code
+var charaterLength = (8>=128) ;
+var charaterArr =[];
+
+var specialCharArr = [ '!', '@' , '#' , '$', '%' , '&' , '*', '?'];
+var lowerCaseArr = [ 'a', 'b' , 'c' , 'd', 'e' , 'f' , 'g', 'h' , 'i' , 'j', 'k' , 'l' ,'m' ,'n' , 'o' , 'p','q' ,'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z',];
+var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7','8', '9',];
+
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener("click", writePassword);
 
+function getPrompts(){
+  charaterLength = parseInt(prompt("How many characters do you want in your password? (8-128"));
+  if(isNaN(charaterLength) || charaterLength< 8 || charaterLength > 128) {
+    alert("Character length has to be a number, 8-128 characters, Please try again");
+  return false;
+  }
+  if (confirm("Include lowercaser letters?")) {
+    charaterArr = charaterArr.concat(lowerCaseArr);
+  }
+  if (confirm("Include uppercase letters?")) {
+    charaterArr = charaterArr.concat(upperCaseArr);
+  }
+  if (confirm("Include numbers?")) {
+    charaterArr = charaterArr.concat(numberArr);
+  }
+  if (confirm("Include special characters?")) {
+    charaterArr = charaterArr.concat(specialCharArr);
+  }
+  return true
+}
+
+function writePassword() {
+var correctPrompts = getPrompts(); 
+var passwordText = document.querySelector("#password");
+
+
+  if (correctPrompts) {
+    var password = generatePassword();
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+
+  return password;
+  }
+
+}
 
 function generatePassword() {
-// 1 password criteria ask
-// 1a length 8-128
-// 1b lower, upper, number, special
-// 2 imput valadtion, a least one 4 loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return"generator password will go here";
+  var password = "";
+  for(var i = 0; i < charaterLength ; i++) {
+    var randomIndex = Math.floor(Math.random() * charaterArr.length);
+   password = password + charaterArr[randomIndex] ;
+  }
+    return password; 
 }
-
-
-
-// Write password to the #password input
-// 3 valid input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-document.querySelector('#password').pass;
-console.log ('#password');
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-// 4 display password
-generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # password-generator-kaf
+
+https://www.w3schools.com/jsref/met_win_prompt.asp
+
+https://www.w3schools.com/js/js_booleans.asp
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+
+https://www.w3schools.com/js/js_arrays.asp
+
+https://www.w3schools.com/js/js_number_methods.asp
+
+https://www.w3schools.com/js/js_numbers.asp    Nan number
+
+https://www.youtube.com/watch?v=v2jfGo7ztm8
+
+https://www.w3schools.com/jsref/jsref_floor.asp
